b071479151da0998673e4cf8e0d6b619e2f4305f
samuraiOrDev <vm.ordiales.garcia@gmail.com>
2024-10-31 22:56:57 +0100

    Creación de logo, metadatas y cambio en el icon

--- src/components/homePage/HeroSection.tsx
+++ src/components/homePage/HeroSection.tsx
@@ -1,5 +1,6 @@
 import { Download, Mail } from "lucide-react"
 import { Button, Section } from "../ui"
+import Logo from "../svg/Logo"
 
 
 const HeroSection = () => {
@@ -7,17 +8,13 @@ const HeroSection = () => {
         <Section>
             <div className="flex flex-col items-center text-center gap-6">
                 {/* Img */}
-                <div className="w-32 h-32 rounded-full bg-secondary overflow-hidden">
-                    <img
-                        src="https://www.claudeusercontent.com/api/placeholder/128/128"
-                        alt="Profile"
-                        className="w-full h-full object-cover"
-                    />
+                <div className="flex items-center justify-center overflow-hidden">
+                    <Logo />
                 </div>
                 {/* Text */}
                 <div className="space-y-4">
                     <h1 className="text-4xl font-bold bg-gradient-to-r from-blue-400 to-violet-400 bg-clip-text text-transparent">
-                        Desarrollador Full Stack
+                        Veriel.Dev
                     </h1>
                     <p className="text-xl text-zinc-400 max-w-2xl">
                         Apasionado por crear experiencias web únicas y resolver problemas complejos a través del código.
--- src/components/layouts/NavBar.tsx
+++ src/components/layouts/NavBar.tsx
@@ -1,14 +1,13 @@
-import { Github, Linkedin, Moon } from "lucide-react"
+import { Github, Linkedin, Mail, Moon } from "lucide-react"
 import { Button } from "../ui"
 
 export const NavBar = () => {
     return (
         <nav className=" sticky top-0 bg-zinc-950/80 backdrop-blur-sm z-70">
-            <div className="max-w-6xl mx-auto px-4 py-4 flex justify-between items-center">
-                <h1 className="text-xl font-bold">VerielDev</h1>
+            <div className="max-w-6xl mx-auto px-4 py-4 flex justify-end items-center">
                 <div className="flex items-center gap-4">
                     <Button variant="ghost">
-                        <Moon className="h-5 w-5" />
+                        <Mail className="h-5 w-5" />
                     </Button>
                     <Button variant="ghost">
                         <Github className="h-5 w-5" />
