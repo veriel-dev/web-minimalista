83ec47b9fff58e306d5a13cd57c0ed24a368325d
samuraiOrDev <vm.ordiales.garcia@gmail.com>
2024-11-01 00:50:49 +0100

    Eliminación de archivos y visualización de stats

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-import { visualizer } from 'rollup-plugin-visualizer';
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),
-    visualizer({
-      open: true,
-      gzipSize: true
-    }),
-  ],
+  plugins: [react(),],
   build: {
     rollupOptions: {
       output: {
