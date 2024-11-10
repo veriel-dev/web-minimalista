37c7bbcb359175d53bee69b28107db9ec120aef6
samuraiOrDev <vm.ordiales.garcia@gmail.com>
2024-11-10 16:09:59 +0100

    Logos para el background

---
+++ src/screens/VerielBackground.tsx
@@ -0,0 +1,12 @@
+import LogoBackgorund from "../components/svg/LogoBackgorund";
+
+const VerielBackground = () => {
+    return (
+        <div className="min-h-screen bg-gray-950/90 flex items-center justify-center text-center p-4 ">
+            <div className="w-[1000px]">
+                <LogoBackgorund />
+            </div>
+        </div>
+    );
+};
+export default VerielBackground
