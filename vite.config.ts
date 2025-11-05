65d4e4f41af7a0bd91388bfd9ad6579bf556208f
veriel-dev <vm.ordiales.garcia@gmail.com>
2025-11-05 16:24:47 +0100

    feat:error source-maps

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,25 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
 // https://vite.dev/config/
 export default defineConfig({
   server: {
     host: '0.0.0.0',
-    port: 5173
+    port: 5173,
   },
-  plugins: [react(),],
+  plugins: [react()],
   build: {
     rollupOptions: {
       output: {
         manualChunks: {
-          'vendor': ['react', 'react-dom'],
-          'layout': ['./src/components/layouts/MainLayout'],
-          'home': ['./src/screens/HomePage'],
-          'cv': ['./src/screens/CurriculumVitaePage'],
-        }
-      }
+          vendor: ['react', 'react-dom'],
+          layout: ['./src/components/layouts/MainLayout'],
+          home: ['./src/screens/HomePage'],
+          cv: ['./src/screens/CurriculumVitaePage'],
+        },
+      },
     },
     chunkSizeWarningLimit: 1000,
-    sourcemap: true,
-  }
-})
+    sourcemap: false,
+  },
+});
