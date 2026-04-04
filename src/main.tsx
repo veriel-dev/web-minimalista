8836bcbc5d71d8b92ed4b95781af14f320397174
veriel-dev <vm.ordiales.garcia@gmail.com>
2026-04-04 22:24:11 +0200

    feat: add console.log veriel

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,32 @@ import './styles/transitions.css';
 import App from './App.tsx';
 import { HelmetProvider } from 'react-helmet-async';
 
+if (import.meta.env.PROD) {
+  const styles = [
+    'color: #a78bfa; font-size: 14px; font-weight: bold; font-family: monospace',
+    'color: #71717a; font-size: 11px; font-family: monospace',
+    'color: #22c55e; font-size: 11px; font-family: monospace',
+  ];
+
+  console.log(
+    `%c
+ ██╗   ██╗███████╗██████╗ ██╗███████╗██╗
+ ██║   ██║██╔════╝██╔══██╗██║██╔════╝██║
+ ██║   ██║█████╗  ██████╔╝██║█████╗  ██║
+ ╚██╗ ██╔╝██╔══╝  ██╔══██╗██║██╔══╝  ██║
+  ╚████╔╝ ███████╗██║  ██║██║███████╗███████╗
+   ╚═══╝  ╚══════╝╚═╝  ╚═╝╚═╝╚══════╝╚══════╝
+`,
+    styles[0],
+  );
+
+  console.log(
+    '%cSi estás leyendo esto, probablemente seas dev. Echa un vistazo al código:',
+    styles[1],
+  );
+  console.log('%chttps://github.com/veriel-dev/web-minimalista', styles[2]);
+}
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <HelmetProvider>
